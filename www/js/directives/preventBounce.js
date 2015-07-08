23c2d58e5486fc67e027856e83db17fe8b10d1d3
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2015-07-08 17:31:14 +0200

    Textseitenupdate (Klassen)

--- www/js/directives/preventBounce.js
+++ www/js/directives/preventBounce.js
@@ -1,22 +1,20 @@
 angular.module('starter.directives')
 
-.directive('preventBounce', ['$ionicGesture', '$ionicSlideBoxDelegate', 
-function ($ionicGesture, $ionicSlideBoxDelegate) {
-  return {
-    restrict: 'A',
-    link    : function (scope, elem) {
-      var reportEvent = function (e) {
-        if (e.target.tagName.toLowerCase() === 'img' || e.target.tagName.toLowerCase() === 'video' || e.target.tagName.toLowerCase() === 'ion-slide') {
-          $ionicSlideBoxDelegate.$getByHandle('pageSlidebox').enableSlide(false);
-        } else {
-          console.log (e.target);
-          $ionicSlideBoxDelegate.$getByHandle('pageSlidebox').enableSlide(true);
-        }
-      };
-      $ionicGesture.on('touch', reportEvent, elem);
-      $ionicGesture.on('release', function() {
-        $ionicSlideBoxDelegate.$getByHandle('pageSlidebox').enableSlide(true);
-      }, elem);
-    }
-  };
+.directive('preventBounce', [
+function () {
+	return {
+		restrict: 'A',
+		link: function (scope, elem) {
+			console.log ('preventBounce loaded');
+			document.addEventListener("touchmove", function(event) {
+
+			   /* if (event.target.tagName != "TEXTAREA") { // Element that you don't want to be prevented default event.
+
+			          event.preventDefault();
+			    }*/
+			    console.log ('stop');
+			    event.preventDefault();
+			});
+		}
+	};
 }]);
